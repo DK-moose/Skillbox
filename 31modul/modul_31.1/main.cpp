a5998ef10fdb7bc33ca0d1dfff0c01bb244f75ff
DK-moose <mitiakolotov@gmail.com>
2022-03-14 19:58:37 +0400

    update modul 31

--- 31modul/modul_31.1/main.cpp
+++ 31modul/modul_31.1/main.cpp
@@ -18,36 +18,66 @@ class shared_ptr_toy
 {
 private:
 	Toy* toy;
+	int *linkCounter = new int(1);
 
 public:
 	shared_ptr_toy(string name)
 	{
 		toy = new Toy(name);
-	}
+	};
 
-	shared_ptr_toy(const shared_ptr_toy& otherToy)
+	shared_ptr_toy(shared_ptr_toy& otherToy)
 	{
 		toy = new Toy(*otherToy.toy);
-	}
+		linkCounter = otherToy.GetLinkCounter();
+		*linkCounter = *linkCounter + 1;
+		otherToy.SetLinkCounter(linkCounter);
+	};
 
-	shared_ptr_toy& operator=(const shared_ptr_toy& otherToy)
+	shared_ptr_toy& operator=(shared_ptr_toy& otherToy)
 	{
 		if (this == &otherToy)
+		{
+			*linkCounter = *linkCounter + 1;
+			otherToy.SetLinkCounter(linkCounter);
 			return *this;
+		}
 		if (toy != nullptr)
+		{
+			*linkCounter = *linkCounter - 1;
+			otherToy.SetLinkCounter(linkCounter);
 			delete toy;
+		}
 		toy = new Toy(*otherToy.toy);
 		return *this;
-	}
+	};
 
 	string GetName()
 	{
 		return toy->GetName();
+	};
+
+	~shared_ptr_toy()
+	{
+		*linkCounter = *linkCounter - 1;
+		delete toy;
+	};
+
+	void SetLinkCounter(int* linkCounter)
+	{
+		this->linkCounter = linkCounter;
 	}
 
-	~shared_ptr_toy() { delete toy; };
-};
+	int* GetLinkCounter()
+	{
+		return linkCounter;
+	}
 
+	void reset()
+	{
+		*linkCounter = 0;
+	}
+};
 
 shared_ptr_toy make_shared_toy()
 {
@@ -64,13 +94,42 @@ shared_ptr_toy make_shared_toy(shared_ptr_toy toy)
 	return shared_ptr_toy(toy);
 }
 
+class Dog
+{
+private:
+	string name;
+	int age;
+	shared_ptr_toy lovelyToy;
+
+public:
+	Dog() : name("SomeName"), age(0), lovelyToy(make_shared_toy()) {};
+	Dog(string name, int age, shared_ptr_toy lovelyToy) : name(name), age(age), lovelyToy(make_shared_toy(lovelyToy)) {};
+	void PrintDog()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Lovely toy: " << lovelyToy.GetName() << endl << endl;
+	};
+};
+
 int main()
 {
-	shared_ptr_toy someToy = make_shared_toy();
-	cout << someToy.GetName() << endl;
 	shared_ptr_toy ball = make_shared_toy("Ball");
-	cout << ball.GetName() << endl;
-	shared_ptr_toy copyBall = make_shared_toy(ball);
-	cout << copyBall.GetName() << endl;
+	shared_ptr_toy bone = make_shared_toy("Bone");
+	Dog *dog1 = new Dog("Sharik", 5, ball);
+	Dog *dog2 = new Dog("Bobik", 3, ball);
+	Dog *dog3 = new Dog("Tuzik", 4, ball);
+	Dog *dog4 = new Dog("Jack", 7, bone);
+	Dog *dog5 = new Dog("Sam", 8, bone);
+
+	delete dog1;
+	delete dog3;
+	delete dog4;
+
+	dog2->PrintDog();
+	dog5->PrintDog();
+
+	ball.reset();
+	bone.reset();
 	return 0;
 }
