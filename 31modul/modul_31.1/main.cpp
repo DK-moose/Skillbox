4d8d16107ba1c71b9fc3d5c75665e09377b16d8b
DK-moose <mitiakolotov@gmail.com>
2022-06-30 15:54:58 +0400

    update 31modul

--- 31modul/modul_31.1/main.cpp
+++ 31modul/modul_31.1/main.cpp
@@ -9,80 +9,76 @@ private:
 	string name;
 
 public:
-	Toy(string name) : name(name) {};
 	Toy() : name("SomeToy") {};
-	string GetName() { return name; };
+	Toy(string name) : name(name) {};
+	void PrintToy() { cout << name; };
 };
 
 class shared_ptr_toy
 {
 private:
 	Toy* toy;
-	int *linkCounter = new int(1);
+	int *linkCounter;
 
 public:
+	shared_ptr_toy()
+	{
+		toy = new Toy();
+		linkCounter = new int(1);
+	};
+
 	shared_ptr_toy(string name)
 	{
 		toy = new Toy(name);
+		linkCounter = new int(1);
 	};
 
-	shared_ptr_toy(shared_ptr_toy& otherToy)
+	shared_ptr_toy(const shared_ptr_toy& otherToy)
 	{
-		toy = new Toy(*otherToy.toy);
-		linkCounter = otherToy.GetLinkCounter();
-		*linkCounter = *linkCounter + 1;
-		otherToy.SetLinkCounter(linkCounter);
+		toy = otherToy.toy;
+		*otherToy.linkCounter = *otherToy.linkCounter + 1;
+		linkCounter = otherToy.linkCounter;
 	};
 
-	shared_ptr_toy& operator=(shared_ptr_toy& otherToy)
+	shared_ptr_toy& operator=(const shared_ptr_toy& otherToy)
 	{
 		if (this == &otherToy)
-		{
-			*linkCounter = *linkCounter + 1;
-			otherToy.SetLinkCounter(linkCounter);
 			return *this;
-		}
 		if (toy != nullptr)
 		{
 			*linkCounter = *linkCounter - 1;
-			otherToy.SetLinkCounter(linkCounter);
-			delete toy;
+			if (linkCounter == 0)
+			{
+				delete toy;
+				delete linkCounter;
+			}
 		}
 		toy = new Toy(*otherToy.toy);
-		return *this;
+		*otherToy.linkCounter = *otherToy.linkCounter + 1;
+		linkCounter = otherToy.linkCounter;
 	};
 
-	string GetName()
+	void PtintSharedPtrToy()
 	{
-		return toy->GetName();
-	};
-
-	~shared_ptr_toy()
-	{
-		*linkCounter = *linkCounter - 1;
-		delete toy;
-	};
-
-	void SetLinkCounter(int* linkCounter)
-	{
-		this->linkCounter = linkCounter;
-	}
-
-	int* GetLinkCounter()
-	{
-		return linkCounter;
+		this->toy->PrintToy();
 	}
 
 	void reset()
 	{
-		*linkCounter = 0;
+		delete toy;
+		delete linkCounter;
 	}
-};
 
-shared_ptr_toy make_shared_toy()
-{
-	return shared_ptr_toy("SomeToy");
-}
+	~shared_ptr_toy()
+	{
+		*linkCounter = *linkCounter - 1;
+		if (linkCounter == 0)
+		{
+			delete toy;
+			delete linkCounter;
+		}
+	};
+};
 
 shared_ptr_toy make_shared_toy(string name)
 {
@@ -102,34 +98,53 @@ private:
 	shared_ptr_toy lovelyToy;
 
 public:
-	Dog() : name("SomeName"), age(0), lovelyToy(make_shared_toy()) {};
-	Dog(string name, int age, shared_ptr_toy lovelyToy) : name(name), age(age), lovelyToy(make_shared_toy(lovelyToy)) {};
+	Dog() : name("SomeName"), age(0), lovelyToy(make_shared_toy("SomeToy")) {};
+	Dog(string name, int age, string toyName) : name(name), age(age), lovelyToy(make_shared_toy(toyName)) {};
+	Dog(string name, int age, shared_ptr_toy toy) : name(name), age(age), lovelyToy(make_shared_toy(toy)) {};
+	
 	void PrintDog()
 	{
 		cout << "Name: " << name << endl;
 		cout << "Age: " << age << endl;
-		cout << "Lovely toy: " << lovelyToy.GetName() << endl << endl;
+		cout << "Lovely toy: ";
+		lovelyToy.PtintSharedPtrToy();
+		cout << endl << endl;
 	};
+
+	void CopyLovelyToy(const Dog& otherDog)
+	{
+		lovelyToy = otherDog.lovelyToy;
+	}
 };
 
 int main()
 {
 	shared_ptr_toy ball = make_shared_toy("Ball");
 	shared_ptr_toy bone = make_shared_toy("Bone");
-	Dog *dog1 = new Dog("Sharik", 5, ball);
-	Dog *dog2 = new Dog("Bobik", 3, ball);
-	Dog *dog3 = new Dog("Tuzik", 4, ball);
-	Dog *dog4 = new Dog("Jack", 7, bone);
-	Dog *dog5 = new Dog("Sam", 8, bone);
 
-	delete dog1;
-	delete dog3;
-	delete dog4;
+	Dog dog_1("Sharik", 10, ball);
+	Dog dog_2("Bobik", 7, bone);
+	Dog dog_3("Tuzik", 8, ball);
+	Dog dog_4("Reks", 9, bone);
+	Dog dog_5("Pushok", 11, ball);
 
-	dog2->PrintDog();
-	dog5->PrintDog();
+	dog_1.PrintDog();
+	dog_2.PrintDog();
+	dog_3.PrintDog();
+	dog_4.PrintDog();
+	dog_5.PrintDog();
+
+	dog_2.CopyLovelyToy(dog_3);
+	dog_4.CopyLovelyToy(dog_3);
+
+	dog_1.PrintDog();
+	dog_2.PrintDog();
+	dog_3.PrintDog();
+	dog_4.PrintDog();
+	dog_5.PrintDog();
 
 	ball.reset();
 	bone.reset();
+
 	return 0;
 }
